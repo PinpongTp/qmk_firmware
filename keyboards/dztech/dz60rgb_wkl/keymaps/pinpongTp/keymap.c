d0993b45ac5a11381a457269b022a03b38fc669d
Pinpong Tongpat <apa.pinpong.t@siampiwat.com>
2023-07-02 16:15:09 +0700

    feat: update layer name

--- keyboards/dztech/dz60rgb_wkl/keymaps/pinpongTp/keymap.c
+++ keyboards/dztech/dz60rgb_wkl/keymaps/pinpongTp/keymap.c
@@ -2,51 +2,61 @@
 
 enum Layer {
     _MAIN = 0,
-    _FUN,
     _MODE,
+    _MEDEA,
+    _ARROW,
     _NUM,
+    _FUN,
     _SYMBOL,
 };
-#define _MN _MAIN
-#define _FN _FUN
-#define _MD _MODE
-#define _NM _NUM
-#define _SB _SYMBOL
-
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_MAIN] = LAYOUT_60_tsangan_hhkb(
-        KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,
-        KC_TAB,          KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
-        LCTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
-        KC_LSFT, LSFT_T(KC_Z),    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  RSFT_T(KC_SLSH), KC_RSFT, MO(_MAIN),
-        _______,     KC_LALT, KC_LGUI,                            KC_SPC,                                      KC_RGUI, MO(_MODE), _______
-    ),
-    [_FUN] = LAYOUT_60_tsangan_hhkb(
-        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______,
-        _______, _______, KC_UP,   _______, _______, _______, KC_CALC, _______, KC_INS,  _______, KC_PSCR, KC_SLCK, KC_PAUS, QK_BOOT,
-        _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGUP,          _______,
-        KC_MPRV,          KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, NK_TOGG, _______, _______, KC_END,  KC_PGDN, KC_MNXT, _______,
-        _______, _______, _______,                            _______,                                     _______, _______, _______
+        KC_GESC,        KC_1,  KC_2,  KC_3,  KC_4,  KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,        KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,
+        KC_TAB,         KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,        KC_LBRC, KC_RBRC, KC_BSPC,
+        LCTL_T(KC_ESC), KC_A,  KC_S,  KC_D,  KC_F,  KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,     KC_QUOT,          KC_ENT,
+        KC_LSFT, LSFT_T(KC_Z), KC_X,  KC_C,  KC_V,  KC_B,    KC_N,    KC_M,    KC_COMM,KC_DOT,RSFT_T(KC_SLSH),KC_RSFT, MO(_MODE),
+        _______,     KC_LALT, KC_LGUI,                            KC_SPC,                        KC_RGUI,   MO(_MEDEA), _______
     ),
     [_MODE] = LAYOUT_60_tsangan_hhkb(
-        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  _______,
-        _______, RGB_TOG, _______, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, RGB_MOD, _______, _______, _______, QK_BOOT,
-        _______, _______, _______, _______, _______, _______, _______, _______, RGB_SPI, RGB_SPD, _______, _______,          _______,
-        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR, _______,
+        _______, _______, KC_UP,   _______, RESET,   _______, _______, _______, _______, _______, _______, KC_UP,   _______, KC_DEL,
+        _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_RGHT,     _______,
+        _______,          _______, _______, _______, QK_BOOT, _______, NK_TOGG, KC_MUTE, KC_VOLD, KC_VOLU, KC_DOWN, _______, _______,
         _______, _______, _______,                            _______,                                     _______, _______, _______
     ),
-    [_NUM] = LAYOUT_60_tsangan_hhkb(
+    [_MEDEA] = LAYOUT_60_tsangan_hhkb(
+        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR,  _______,
+        _______, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, _______, _______, _______, _______, _______, _______, KC_PAUS, KC_DEL,
+        _______, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, _______, _______, _______, KC_SLCK, _______, _______,          _______,
+        _______,          _______, _______, _______, QK_BOOT, _______, _______, _______, KC_MPRV, KC_MNXT, _______, _______, _______,
+        _______, _______, _______,                            _______,                                     _______, _______, _______
+    ),
+    [_ARROW] = LAYOUT_60_tsangan_hhkb( // by combo SD
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
-        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, KC_INS, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, _______, _______,      _______,
+        _______,          _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGUP, KC_PGDN, KC_END, _______, _______,
+        _______, _______, _______,                            _______,                                     _______, _______, _______
+    ),
+    [_NUM] = LAYOUT_60_tsangan_hhkb( // by combo DF
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, KC_7,    KC_8,    KC_9,    _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, KC_4,    KC_5,    KC_6,    _______,          _______,
+        _______,          _______, _______, _______, _______, _______, KC_0,    KC_1,    KC_2,    KC_3,    _______, _______, _______,
+        _______, _______, _______,                            KC_0,                                     _______, _______, _______
+    ),
+    [_FUN] = LAYOUT_60_tsangan_hhkb( // by combo EF
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, KC_F7,   KC_F8,   KC_F9,   KC_F11,  _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, KC_F4,   KC_F5,   KC_F6,   KC_F12,          _______,
+        _______,          _______, _______, _______, _______, _______, _______, KC_F1,   KC_F2,   KC_F3,   KC_F13,  _______, _______,
         _______, _______, _______,                            _______,                                     _______, _______, _______
     ),
     [_SYMBOL] = LAYOUT_60_tsangan_hhkb(
-        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  _______,
-        _______, RGB_TOG, _______, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, RGB_MOD, _______, _______, _______, RESET,
-        _______, _______, _______, _______, _______, _______, _______, _______, RGB_SPI, RGB_SPD, _______, _______,          _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, KC_PEQL, _______, _______, _______, _______, _______, _______, _______,          _______,
         _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______,                            _______,                                     _______, _______, _______
     )
@@ -57,6 +67,7 @@ enum combos {
     XC_TAB,
     KL_ENT,
     IO_DEL,
+    SD_ARW,
     COMBO_LENGTH
 };
 uint16_t COMBO_LEN = COMBO_LENGTH;
@@ -64,10 +75,12 @@ const uint16_t PROGMEM we_combo[] = {KC_W, KC_E, COMBO_END};
 const uint16_t PROGMEM xc_combo[] = {KC_X, KC_C, COMBO_END};
 const uint16_t PROGMEM kl_combo[] = {KC_K, KC_L, COMBO_END};
 const uint16_t PROGMEM io_combo[] = {KC_I, KC_O, COMBO_END};
+const uint16_t PROGMEM sd_combo[] = {KC_S, KC_D, COMBO_END};
 
 combo_t key_combos[] = {
   [WE_ESC] = COMBO(we_combo, KC_ESC),
   [XC_TAB] = COMBO(xc_combo, KC_TAB),
   [KL_ENT] = COMBO(kl_combo, KC_PENT),
   [IO_DEL] = COMBO(io_combo, KC_BSPC),
+  [SD_ARW] = COMBO(sd_combo, MO(_ARROW)),
 };
