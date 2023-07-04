9ea0546800ab1ad05d3f2368cb76bb2ce1ce9ca7
Pinpong Tongpat <pinpong.tp@gmail.com>
2023-07-05 00:01:39 +0700

    feat: change to GSCA

--- keyboards/dztech/dz60rgb_wkl/keymaps/pinpongTp/keymap.c
+++ keyboards/dztech/dz60rgb_wkl/keymaps/pinpongTp/keymap.c
@@ -12,14 +12,14 @@ enum Layer {
 
 // Left-hand home row mods
 #define HOME_A LGUI_T(KC_A)
-#define HOME_S LOPT_T(KC_S)
+#define HOME_S LSFT_T(KC_S)
 #define HOME_D LCTL_T(KC_D)
-#define HOME_F LSFT_T(KC_F)
+#define HOME_F LOPT_T(KC_F)
 
 // Right-hand home row mods
-#define HOME_J RSFT_T(KC_J)
+#define HOME_J ROPT_T(KC_J)
 #define HOME_K RCTL_T(KC_K)
-#define HOME_L ROPT_T(KC_L)
+#define HOME_L RSFT_T(KC_L)
 #define HOME_SCLN RGUI_T(KC_SCLN)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
