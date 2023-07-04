776e2377e2e66e8b9ffec93839636924027eb63f
Pinpong Tongpat <pinpong.tp@gmail.com>
2023-07-05 00:51:07 +0700

    feat: update to SGCA

--- keyboards/dztech/dz60rgb_wkl/keymaps/pinpongTp/keymap.c
+++ keyboards/dztech/dz60rgb_wkl/keymaps/pinpongTp/keymap.c
@@ -12,14 +12,14 @@ enum Layer {
 
 // Left-hand home row mods
 #define HOME_A LSFT_T(KC_A)
-#define HOME_S LCTL_T(KC_S)
-#define HOME_D LOPT_T(KC_D)
-#define HOME_F LGUI_T(KC_F)
+#define HOME_S LGUI_T(KC_S)
+#define HOME_D LCTL_T(KC_D)
+#define HOME_F LOPT_T(KC_F)
 
 // Right-hand home row mods
-#define HOME_J RGUI_T(KC_J)
-#define HOME_K ROPT_T(KC_K)
-#define HOME_L RCTL_T(KC_L)
+#define HOME_J ROPT_T(KC_J)
+#define HOME_K RCTL_T(KC_K)
+#define HOME_L RGUI_T(KC_L)
 #define HOME_SCLN RSFT_T(KC_SCLN)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
