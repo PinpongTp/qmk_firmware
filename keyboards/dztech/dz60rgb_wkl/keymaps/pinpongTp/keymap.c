1a194a3e4d26d34b2846ea99c68429691b21a32b
Pinpong Tongpat <pinpong.tp@gmail.com>
2023-07-04 22:38:25 +0700

    feat: change to GACS

--- keyboards/dztech/dz60rgb_wkl/keymaps/pinpongTp/keymap.c
+++ keyboards/dztech/dz60rgb_wkl/keymaps/pinpongTp/keymap.c
@@ -11,16 +11,16 @@ enum Layer {
 };
 
 // Left-hand home row mods
-#define HOME_A LSFT_T(KC_A)
-#define HOME_S LCTL_T(KC_S)
-#define HOME_D LOPT_T(KC_D)
-#define HOME_F LGUI_T(KC_F)
+#define HOME_A LGUI_T(KC_A)
+#define HOME_S LOPT_T(KC_S)
+#define HOME_D LCTL_T(KC_D)
+#define HOME_F LSFT_T(KC_F)
 
 // Right-hand home row mods
-#define HOME_J RGUI_T(KC_J)
-#define HOME_K ROPT_T(KC_K)
-#define HOME_L RCTL_T(KC_L)
-#define HOME_SCLN RSFT_T(KC_SCLN)
+#define HOME_J RSFT_T(KC_J)
+#define HOME_K RCTL_T(KC_K)
+#define HOME_L ROPT_T(KC_L)
+#define HOME_SCLN RGUI_T(KC_SCLN)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_MAIN] = LAYOUT_60_tsangan_hhkb(
