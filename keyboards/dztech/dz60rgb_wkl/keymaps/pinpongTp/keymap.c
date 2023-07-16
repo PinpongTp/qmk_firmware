08dc0c60a7f71eb3fbc96a51ebd33af5c558e3f7
Pinpong Tongpat <pinpong.tp@gmail.com>
2023-07-16 22:42:23 +0700

    feat: add number key

--- keyboards/dztech/dz60rgb_wkl/keymaps/pinpongTp/keymap.c
+++ keyboards/dztech/dz60rgb_wkl/keymaps/pinpongTp/keymap.c
@@ -25,9 +25,9 @@ enum Layer {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_MAIN] = LAYOUT_60_tsangan_hhkb(
-        KC_GESC,   KC_1,  KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,        KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,
-        KC_TAB,    KC_Q,  KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,        KC_LBRC, KC_RBRC, KC_BSPC,
-        CTL_T(KC_ESC),HOME_A,HOME_S,HOME_D, HOME_F,  KC_G,    KC_H,    HOME_J,  HOME_K,  HOME_L,  HOME_SCLN,   KC_QUOT, KC_ENT,
+        KC_GESC,   KC_1,KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,        KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,
+        KC_TAB,    LT(_NUM,KC_Q),  KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    LT(_NUM,KC_P),  KC_LBRC, KC_RBRC, KC_BSPC,
+        CTL_T(KC_ESC),HOME_A,HOME_S,HOME_D, HOME_F,  LT(_SYM,KC_G), LT(_SYM,KC_H),    HOME_J,  HOME_K,  HOME_L,  HOME_SCLN,   KC_QUOT, KC_ENT,
         KC_LSFT,LT(_SYM,KC_Z),KC_X,  KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,KC_DOT,LT(_SYM,KC_SLSH),KC_RSFT, MO(_MODE),
         _______,     KC_LALT, KC_LGUI,                            KC_SPC,                         KC_RGUI,     MO(_MODE), _______
     ),
@@ -54,16 +54,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [_NUM] = LAYOUT_60_tsangan_hhkb( // by combo DF
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, KC_7,    KC_8,    KC_9,    _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, KC_4,    KC_5,    KC_6,    _______, _______,          _______,
-        _______, _______, _______, _______, _______, _______, KC_0,    KC_1,    KC_2,    KC_3,    _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, KC_PLUS, KC_7,    KC_8,    KC_9,    _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, KC_MINS, KC_4,    KC_5,    KC_6,    KC_DOT,  _______,          _______,
+        _______, _______, _______, _______, _______, _______, KC_ASTR, KC_1,    KC_2,    KC_3,    _______, _______, _______,
         _______, _______, _______,                            KC_0,                                     _______, _______, _______
     ),
     [_FUN] = LAYOUT_60_tsangan_hhkb( // by combo EF
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, KC_MUTE, KC_F7,   KC_F8,   KC_F9,   KC_F11,  _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, KC_VOLU, KC_F4,   KC_F5,   KC_F6,   KC_F12,  _______,        _______,
-        _______, _______, _______, _______, _______, _______, KC_VOLD, KC_F1,   KC_F2,   KC_F3,   KC_F13,  _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, KC_F7,   KC_F8,   KC_F9,   KC_F11,  _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,SCMD(KC_4),KC_F4,  KC_F5,   KC_F6,   KC_F12,  _______,        _______,
+        _______, _______, _______, _______, _______, _______, _______, KC_F1,   KC_F2,   KC_F3,   KC_F13,  _______, _______,
         _______, _______, _______,                            _______,                                     _______, _______, _______
     ),
     [_SYM] = LAYOUT_60_tsangan_hhkb(
@@ -116,14 +116,15 @@ enum combos {
     LSEMI_TAB,
     KL_ENT,
     IO_DEL,
-    JL_RBRC,
-    UO_RPRN,
-    SF_LBRC,
-    WR_LPRN,
+    /*JL_RBRC,*/
+    /*UO_RPRN,*/
+    /*SF_LBRC,*/
+    /*WR_LPRN,*/
     FJ_CAPS,
     SD_ARW,
     DF_NUM,
     EF_FUN,
+    QR_FUN,
     AS_MOU,
     LANG,
     LANG_A,
@@ -136,14 +137,15 @@ const uint16_t PROGMEM xc_combo[] = {KC_X,   KC_C, COMBO_END};
 const uint16_t PROGMEM lscln_combo[] = {HOME_L,   HOME_SCLN, COMBO_END};
 const uint16_t PROGMEM kl_combo[] = {HOME_K, HOME_L, COMBO_END};
 const uint16_t PROGMEM io_combo[] = {KC_I,   KC_O, COMBO_END};
-const uint16_t PROGMEM jl_combo[] = {HOME_J,   HOME_L, COMBO_END};
-const uint16_t PROGMEM uo_combo[] = {KC_U,   KC_O, COMBO_END};
-const uint16_t PROGMEM sf_combo[] = {HOME_S,   HOME_F, COMBO_END};
-const uint16_t PROGMEM wr_combo[] = {KC_W,   KC_R, COMBO_END};
+/*const uint16_t PROGMEM jl_combo[] = {HOME_J,   HOME_L, COMBO_END};*/
+/*const uint16_t PROGMEM uo_combo[] = {KC_U,   KC_O, COMBO_END};*/
+/*const uint16_t PROGMEM sf_combo[] = {HOME_S,   HOME_F, COMBO_END};*/
+/*const uint16_t PROGMEM wr_combo[] = {KC_W,   KC_R, COMBO_END};*/
 const uint16_t PROGMEM FJ_combo[] = {HOME_F,   HOME_J, COMBO_END};
 const uint16_t PROGMEM sd_combo[] = {HOME_S, HOME_D, COMBO_END};
 const uint16_t PROGMEM df_combo[] = {HOME_D, HOME_F, COMBO_END};
 const uint16_t PROGMEM ef_combo[] = {KC_E,   HOME_F, COMBO_END};
+const uint16_t PROGMEM qr_combo[] = {LT(_NUM,KC_Q), KC_R, COMBO_END};
 const uint16_t PROGMEM as_combo[] = {HOME_A,   HOME_S, COMBO_END};
 const uint16_t PROGMEM lang_combo[] = {KC_COMM,KC_DOT, COMBO_END};
 const uint16_t PROGMEM lang_a_combo[] = {KC_M,KC_COMM,KC_DOT, COMBO_END};
@@ -154,14 +156,15 @@ combo_t key_combos[] = {
   [LSEMI_TAB] = COMBO(lscln_combo, KC_TAB),
   [KL_ENT] = COMBO(kl_combo, KC_PENT),
   [IO_DEL] = COMBO(io_combo, KC_BSPC),
-  [JL_RBRC] = COMBO(jl_combo, KC_RBRC),
-  [UO_RPRN] = COMBO(uo_combo, KC_RPRN),
-  [SF_LBRC] = COMBO(sf_combo, KC_LBRC),
-  [WR_LPRN] = COMBO(wr_combo, KC_LPRN),
+  /*[JL_RBRC] = COMBO(jl_combo, KC_RBRC),*/
+  /*[UO_RPRN] = COMBO(uo_combo, KC_RPRN),*/
+  /*[SF_LBRC] = COMBO(sf_combo, KC_LBRC),*/
+  /*[WR_LPRN] = COMBO(wr_combo, KC_LPRN),*/
   [FJ_CAPS] = COMBO(FJ_combo, KC_CAPS),
   [SD_ARW] = COMBO(sd_combo, MO(_ARROW)),
   [DF_NUM] = COMBO(df_combo, MO(_NUM)),
   [EF_FUN] = COMBO(ef_combo, MO(_FUN)),
+  [QR_FUN] = COMBO(qr_combo, MO(_FUN)),
   [AS_MOU] = COMBO(as_combo, MO(_MOU)),
   [LANG] = COMBO(lang_combo, C(KC_SPC)),
   [LANG_A] = COMBO(lang_a_combo, LCA(KC_SPC)),
