6f998f9554a427dbc8cd82b6dcaa12fa26c657b5
Pinpong Tongpat <pinpong.tp@gmail.com>
2023-07-30 14:52:27 +0700

    feat: add nomod layer

--- keyboards/dztech/dz60rgb_wkl/config.h
+++ keyboards/dztech/dz60rgb_wkl/config.h
@@ -11,3 +11,7 @@
 
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
 #define QUICK_TAP_TERM 0
+#undef RGBLIGHT_EFFECT_BREATHING
+#undef RGBLIGHT_EFFECT_KNIGHT
+#undef RGBLIGHT_EFFECT_SNAKE
+#undef RGBLIGHT_EFFECT_TWINKLE
--- keyboards/dztech/dz60rgb_wkl/keymaps/pinpongTp/keymap.c
+++ keyboards/dztech/dz60rgb_wkl/keymaps/pinpongTp/keymap.c
@@ -2,7 +2,7 @@
 
 enum Layer {
     _MAIN = 0,
-    _MODE,
+    _NOMOD,
     _MEDEA,
     _ARROW,
     _NUM,
@@ -31,21 +31,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GESC,   KC_1,KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,        KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,
         KC_TAB,    LT(_NUM,KC_Q),  KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    LT(_NUM,KC_P),  KC_LBRC, KC_RBRC, KC_BSPC,
         CTL_T(KC_ESC),HOME_A,HOME_S,HOME_D, HOME_F,  LT(_NUM,KC_G), KC_H,    HOME_J,  HOME_K,  HOME_L,  HOME_SCLN,   KC_QUOT, KC_ENT,
-        KC_LSFT,LT(_SYM,KC_Z),KC_X,  KC_C,    KC_V,    KC_B,    KC_N,    LT(_MEDEA,KC_M),  KC_COMM,KC_DOT,LT(_SYM,KC_SLSH),KC_RSFT, MO(_MODE),
-        _______,     KC_LALT, KC_LGUI,                            KC_SPC,                         KC_RGUI,     MO(_MODE), _______
+        KC_LSFT,LT(_SYM,KC_Z),LT(_NUM,KC_X),  KC_C,    KC_V,    KC_B,    KC_N,    LT(_MEDEA,KC_M),  KC_COMM,LT(_NUM,KC_DOT),LT(_SYM,KC_SLSH),KC_RSFT, MO(_MEDEA),
+        _______,     KC_LALT, KC_LGUI,                            KC_SPC,                         KC_RGUI,     MO(_MEDEA), _______
     ),
-    [_MODE] = LAYOUT_60_tsangan_hhkb(
-        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR, _______,
-        _______, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, KC_BRK, _______, _______, _______, _______, KC_UP,   KC_PAUS, KC_DEL,
-        _______, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, KC_SCRL, _______, KC_MPRV, KC_MNXT, KC_LEFT, KC_RGHT,     _______,
-        _______, _______, _______, _______, _______, QK_BOOT, NK_TOGG, KC_MUTE, KC_VOLD, KC_VOLU, KC_DOWN, _______, _______,
-        _______, _______, _______,                            _______,                                     _______, _______, _______
+    [_NOMOD] = LAYOUT_60_tsangan_hhkb(
+        KC_ESC,   KC_1,KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,        KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,
+        KC_TAB,    KC_Q,  KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,      KC_LBRC, KC_RBRC, KC_BSPC,
+        KC_LCTL,    KC_A,  KC_S,    KC_D,    KC_F,     KC_G, KC_H,      KC_J,     KC_K,    KC_L,   KC_SCLN,   KC_QUOT, KC_ENT,
+        KC_LSFT,     KC_Z,  KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    LT(_MEDEA,KC_M),  KC_COMM,    KC_DOT,LT(_SYM,KC_SLSH),KC_RSFT, MO(_MEDEA),
+        _______,     KC_LALT, KC_LGUI,                            KC_SPC,                         KC_RGUI,     MO(_MEDEA), _______
     ),
     [_MEDEA] = LAYOUT_60_tsangan_hhkb(
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR, _______,
         _______, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, KC_BRK, _______, _______, _______, _______, KC_UP,   KC_PAUS, KC_DEL,
         _______, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, KC_SCRL, _______, KC_MPRV, KC_MNXT, KC_LEFT, KC_RGHT,     _______,
-        _______, _______, _______, _______, _______, QK_BOOT, NK_TOGG, KC_MUTE, KC_VOLD, KC_VOLU, KC_DOWN, _______, _______,
+        _______, TG(_NOMOD), _______, _______, _______, QK_BOOT, NK_TOGG, KC_MUTE, KC_VOLD, KC_VOLU, KC_DOWN, _______, _______,
         _______, _______, _______,                            _______,                                     _______, _______, _______
     ),
     [_ARROW] = LAYOUT_60_tsangan_hhkb( // by combo SD
@@ -133,7 +133,7 @@ enum combos {
 
 uint16_t COMBO_LEN = COMBO_LENGTH;
 const uint16_t PROGMEM we_combo[] = {KC_W,   KC_E, COMBO_END};
-const uint16_t PROGMEM xc_combo[] = {KC_X,   KC_C, COMBO_END};
+const uint16_t PROGMEM xc_combo[] = {LT(_NUM,KC_X),   KC_C, COMBO_END};
 const uint16_t PROGMEM lscln_combo[] = {HOME_L,   HOME_SCLN, COMBO_END};
 const uint16_t PROGMEM kl_combo[] = {HOME_K, HOME_L, COMBO_END};
 const uint16_t PROGMEM r_tab_combo[] = {KC_U, KC_I, KC_O, HOME_SCLN, COMBO_END};
